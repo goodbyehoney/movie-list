a4ba475c51af6a42684711dfa7d0d763eca30d69
Yevhen Marchukov <evh.gbh@gmail.com>
2023-02-18 22:09:48 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from "react";
+import axios from "axios";
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div>
-      Holla
-    </div>
-  );
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render ( ) {
+    const {isLoading} = this.state;
+    return <div> {isLoading ? "Loading..." : "Сторінка завантажена"}</div>
+  }
+
 }
 
 export default App;
